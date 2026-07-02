c373d7462cd47aace0d29f334b70e061e27e8954
Akshit Bhandula <akshitbhandula@gmail.com>
2026-07-02 21:22:09 +0530

    fixed middleware

--- src/middlewares/validate.middleware.ts
+++ src/middlewares/validate.middleware.ts
@@ -10,9 +10,20 @@ export const validateRequest = (schema: ZodSchema) => {
         query: req.query,
         params: req.params,
       }) as any;
-      req.body = validated.body;
-      req.query = validated.query;
-      req.params = validated.params as any;
+      if (validated.body !== undefined) {
+        req.body = validated.body;
+      }
+      if (validated.query !== undefined) {
+        Object.defineProperty(req, 'query', {
+          value: validated.query,
+          enumerable: true,
+          configurable: true,
+          writable: true,
+        });
+      }
+      if (validated.params !== undefined) {
+        req.params = validated.params as any;
+      }
       return next();
     } catch (error) {
       if (error instanceof ZodError) {
