71b3a620650efa9a9965939d95c7db3ce632c8f1
Akshit Bhandula <akshitbhandula@gmail.com>
2026-07-13 01:47:07 +0530

    Remove deprecated get_users utility script

--- get_users.ts
+++
@@ -1,7 +0,0 @@
-import { PrismaClient } from '@prisma/client'; 
-const prisma = new PrismaClient(); 
-async function run() { 
-  const users = await prisma.user.findMany({ include: { role: true } }); 
-  console.log(users.map(u => `${u.phone} - ${u.role.name}`)); 
-} 
-run();
