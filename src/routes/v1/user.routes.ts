e6fc1952fec76b3ce504cb4a61f20e78e6e31968
Akshit Bhandula <akshitbhandula@gmail.com>
2026-07-12 23:48:51 +0530

    Update user module API endpoints for v1 routing

--- src/routes/v1/user.routes.ts
+++ src/routes/v1/user.routes.ts
@@ -2,7 +2,7 @@ import { Router } from 'express';
 import { getUsers, createUser, updateUser, resetPassword } from '../../controllers/user.controller';
 import { protect } from '../../middlewares/auth.middleware';
 import { validateRequest } from '../../middlewares/validate.middleware';
-import { updateUserSchema, resetPasswordSchema } from '../../validations/user.validation';
+import { createUserSchema, updateUserSchema, resetPasswordSchema } from '../../validations/user.validation';
 import { authorize } from '../../middlewares/rbac.middleware';
 import { catchAsync } from '../../utils/catchAsync';
 
@@ -10,7 +10,8 @@ const router = Router();
 router.use(protect);
 router.use(authorize('Super Admin', 'City Manager'));
 router.get('/', catchAsync(getUsers));
-router.post('/', catchAsync(createUser));
+router.post('/', validateRequest(createUserSchema), catchAsync(createUser));
 router.patch('/:id', validateRequest(updateUserSchema), catchAsync(updateUser));
 router.patch('/:id/reset-password', validateRequest(resetPasswordSchema), catchAsync(resetPassword));
 export default router;
+
