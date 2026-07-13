d82da3e67de4b9e026c3e20c3d46615b675b21eb
Akshit Bhandula <akshitbhandula@gmail.com>
2026-07-13 20:49:33 +0530

    Fix field constraints inside city request validation schema

--- src/validations/city.validation.ts
+++ src/validations/city.validation.ts
@@ -4,6 +4,7 @@ export const updateCitySchema = z.object({
   params: z.object({ id: z.string().uuid('Invalid city ID format') }),
   body: z.object({
     name: z.string().min(1, 'Name cannot be empty').optional(),
+    state: z.string().min(1, 'State cannot be empty').optional(),
     is_active: z.boolean().optional(),
   }).strict()
 });
